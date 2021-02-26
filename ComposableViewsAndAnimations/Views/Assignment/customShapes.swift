4f4a4742baa8423a89b82ab4178185e2f6ca9a51
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-26 14:02:24 -0500

    started to define star

---
+++ ComposableViewsAndAnimations/Views/Assignment/customShapes.swift
@@ -0,0 +1,12 @@
+//
+//  customShapes.swift
+//  ComposableViewsAndAnimations
+//
+//  Created by Harris-Stoertz, Rowan on 2021-02-26.
+//
+
+import Foundation
+
+struct Star: Shape {
+    let size
+}
