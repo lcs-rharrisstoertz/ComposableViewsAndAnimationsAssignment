99d93073fab77c02def08e9abd07271be7d53a62
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-27 11:59:43 -0500

    added instructions to create paths between points

--- ComposableViewsAndAnimations/Views/Assignment/customShapes.swift
+++ ComposableViewsAndAnimations/Views/Assignment/customShapes.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 struct Star: Shape {
-    let starRadius: CGFloat
     func path(in rect: CGRect) -> Path {
         var path = Path()
+        //defining points based on coordinates found by drawing star on graph paper
         let A = CGPoint (x: 0.5 * rect.width, y: 0)
         let B = CGPoint (x: 0.65 * rect.width, y: 0.3 * rect.height)
         let C = CGPoint (x: 0.975 * rect.width, y: 0.35 * rect.height)
@@ -21,5 +21,18 @@ struct Star: Shape {
         let H = CGPoint (x: 0.25 * rect.width, y: 0.575 * rect.height)
         let I = CGPoint (x: 0.025 * rect.width, y: 0.35 * rect.height)
         let J = CGPoint (x: 0.35 * rect.width, y: 0.3 * rect.height)
+        //instructions for it to move between the points in order
+        path.move(to: A)
+        path.addLine(to: B)
+        path.addLine(to: C)
+        path.addLine(to: D)
+        path.addLine(to: E)
+        path.addLine(to: F)
+        path.addLine(to: G)
+        path.addLine(to: H)
+        path.addLine(to: I)
+        path.addLine(to: J)
+        path.addLine(to: A)
+        return path
     }
 }
